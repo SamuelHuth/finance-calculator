e4e813558b78ef1d6ce9a7818a9dfdaef1578186
Samuel Huth <sam@stafflink.com.au>
2021-06-29 15:47:27 +1000

    Plugin Setup

--- finance-calculator/README.md
+++ finance-calculator/README.md
@@ -2,3 +2,16 @@
 __By Samuel Huth__
 
 ## The plugin
+
+### Work in Progress
+
+#### Features
+- Loan Repayment Calculator
+- Stamp Duty Calculator
+- Submit form to send email to agent
+- Save form submits to a database
+
+
+### DevLog
+__29/06/2021__
+Initial development startup. Generating Github repos and baseline plugin structure
--- finance-calculator/finance-calculator.php
+++ finance-calculator/finance-calculator.php
@@ -2,21 +2,21 @@
 /**
  * Plugin Name
  *
- * @package           PluginPackage
- * @author            Your Name
- * @copyright         2019 Your Name or Company Name
+ * @package           FinanceCalculator
+ * @author            Samuel Huth
+ * @copyright         2021 Samuel Huth
  * @license           GPL-2.0-or-later
  *
  * @wordpress-plugin
- * Plugin Name:       Plugin Name
- * Plugin URI:        https://example.com/plugin-name
- * Description:       Description of the plugin.
+ * Plugin Name:       Finance Calculator
+ * Plugin URI:        https://github.com/SamuelHuth/finance-calculator
+ * Description:       A simple plugin that will calculate loan totals, and stamp duty. Will have functionality to send emails of form submits, and save data to the database
  * Version:           1.0.0
  * Requires at least: 5.2
  * Requires PHP:      7.2
- * Author:            Your Name
- * Author URI:        https://example.com
- * Text Domain:       plugin-slug
+ * Author:            Samuel Huth
+ * Author URI:        https://github.com/SamuelHuth
+ * Text Domain:       finance-calculator
  * License:           GPL v2 or later
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  */
